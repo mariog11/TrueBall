c7a76be74a94e34259bb3c80376b5435c4a2cb97
Alexander Cardaras <alexandercardaras@gmail.com>
2019-06-14 17:48:36 -0700

    Added Home-Field Advantage

--- Main.py
+++ Main.py
@@ -2,17 +2,59 @@
 
 import Parser
 import Ranking
+import matplotlib.pyplot as mpl
 
-filepath = 'MLB_2018.txt'
+# filepath = 'MLB_2017.txt' # 205
+filepath = 'MLB_2018.txt' # 183
+
 
 fp = open(filepath)
 
-day = 0
 
-for i in range(0, 150):
+total_count = 0
+total_correct = 0
+average_points = []
+discrete_points = []
+middle_points = [] # for drawing a line at the 0.5 level
+x_values = []
+labels = ["Daily ratio of successful guesses", "Average ratio of successful guesses"]
+starting_day = 20
+for i in range(0, 100):
     day = Parser.getNextDay(fp)
-    Ranking.addEntry(day)
+    middle_points.append(0.5)
+    if i > starting_day:
+        correct, count = Ranking.predictOutcomesByProbability(day)
+        total_correct = total_correct + correct
+        total_count = total_count + count
+        average_points.append(total_correct / total_count)
+        discrete_points.append(correct / count)
+        x_values.append(i)
+        print(total_correct, "/", total_count, (total_correct / total_count))
 
-Ranking.printTeamRanks()
+    Ranking.addEntry(day)
+    # Ranking.printHmAwayProb()
+
+
+
+mpl.ylim([0.1, 0.9])
+print(total_correct, "/", total_count, (total_correct/total_count))
+
+mpl.plot(x_values, discrete_points)
+mpl.plot(x_values, average_points)
+mpl.legend(labels, loc='upper center', bbox_to_anchor=(0.5, -0.12), shadow=True, ncol=1)
+# mpl.plot(x_values, middle_points)
+mpl.ylabel('Probability of guessing correctly')
+mpl.title('MLB 2018 Unique Home-Field Advantage')
+mpl.xlabel('Day')
+
+mpl.savefig("MLB 2018 Unique Home-Field Advantage", dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format=None,
+        transparent=False, bbox_inches='tight', pad_inches=1,
+        frameon=None, metadata=None)
+
+# mpl.show()
+
+
+# Ranking.printTeamRanks()
 games = Parser.getNextDay(fp)
-Ranking.predictOutcomesByRating(games)
+# Ranking.predictOutcomesByRating(games)
--- Ranking.py
+++ Ranking.py
@@ -7,6 +7,8 @@ starting_rating = 25
 alice, bob = Rating(25), Rating(30)  # assign Alice and Bob's ratings
 
 teams = {}
+teams_home_prob = {}
+teams_away_prob = {}
 teamNames = []
 
 
@@ -14,20 +16,28 @@ def addSingleEntry(team1, team2):
     if not teamNames.__contains__(team1[0]):
         teamNames.append(team1[0])
         teams[team1[0]] = [Rating()]
+        teams_home_prob[team1[0]] = [0, 0]
+        teams_away_prob[team1[0]] = [0, 0]
 
     if not teamNames.__contains__(team2[0]):
         teamNames.append(team2[0])
         teams[team2[0]] = [Rating()]
+        teams_home_prob[team2[0]] = [0, 0]
+        teams_away_prob[team2[0]] = [0, 0]
 
     if team1[1] > team2[1]:
         (new_r1,), (new_r2,) = trueskill.rate([teams[team1[0]], teams[team2[0]]], ranks=[0, 1])
         teams[team1[0]] = [new_r1]
         teams[team2[0]] = [new_r2]
+        teams_home_prob[team2[0]] = [teams_home_prob[team2[0]][0], teams_home_prob[team2[0]][1] + 1]
+        teams_away_prob[team1[0]] = [teams_away_prob[team1[0]][0] + 1, teams_away_prob[team1[0]][1] + 1]
 
     if team1[1] < team2[1]:
         (new_r1,), (new_r2,) = trueskill.rate([teams[team1[0]], teams[team2[0]]], ranks=[1, 0])
         teams[team1[0]] = [new_r1]
         teams[team2[0]] = [new_r2]
+        teams_home_prob[team2[0]] = [teams_home_prob[team2[0]][0] + 1, teams_home_prob[team2[0]][1] + 1]
+        teams_away_prob[team1[0]] = [teams_away_prob[team1[0]][0], teams_away_prob[team1[0]][1] + 1]
 
     if team1[1] == team2[1]:
         (new_r1,), (new_r2,) = trueskill.rate([teams[team1[0]], teams[team2[0]]], ranks=[0, 0])
@@ -43,22 +53,42 @@ def addEntry(games):
 
 
 def predictOutcomesByProbability(games):
+    correct_guesses = 0
+    total_guesses = 0
     for game in games:
         team1 = game[0]
         team2 = game[1]
 
-        prob = "{0:.2f}".format(win_probability(teams[team1[0]], teams[team2[0]]))
-        print(team1[0], "has a", prob, "chance of beating", team2[0])
+        total_guesses = total_guesses + 1
+
+        # team2 has a record of 4 wins out of 6 home games
+        # team2's home field advantage is (4/6) - 0.5
+        #                               = 0.66 - 0.5
+        #                               = 0.16
+        # home_advantage = getHomeWinRatio()-0.5
+        home_advantage = getHomeWinRatioTeam(team2)-0.5
+        # home_advantage = 0
+
+        # probability of team1 winning is their rating - team2's home field advantage
+        prob = win_probability(teams[team1[0]], teams[team2[0]]) - home_advantage
+
+        # print(team1[0], "has a", prob, "chance of beating", team2[0])
         if team1[1] > team2[1]:
-            print(team1[0], "beat", team2[0], team1[1], "to", team2[1])
+            # print(team1[0], "beat", team2[0], team1[1], "to", team2[1])
+            if float(prob) > 0.5:
+                correct_guesses = correct_guesses + 1
 
-        if team1[1] < team2[1]:
-            print(team1[0], "lost to", team2[0], team1[1], "to", team2[1])
+        elif team1[1] < team2[1]:
+            # print(team1[0], "lost to", team2[0], team1[1], "to", team2[1])
+            if float(prob) < 0.5:
+                correct_guesses = correct_guesses + 1
 
-        if team1[1] == team2[1]:
-            print(team1[0], "tied", team2[0], team1[1], "to", team2[1])
+        elif team1[1] == team2[1]:
+            # print(team1[0], "tied", team2[0], team1[1], "to", team2[1])
+            if float(prob) == 0.5:
+                correct_guesses = correct_guesses + 1
 
-        print("")
+    return correct_guesses, total_guesses
 
 
 def predictOutcomesByRating(games):
@@ -96,8 +126,66 @@ def printTeamRanks():
         print(name, "\t", teams[name])
 
 
-def win_probability(team1, team2):
-    delta_mu = sum(r.mu for r in team1) - sum(r.mu for r in team2)
+def getHomeWinRatio():
+    total_wins = 0
+    total_games = 0
+
+    for name in teamNames:
+        total_wins = total_wins + teams_home_prob[name][0]
+        total_games = total_games + teams_home_prob[name][1]
+
+    return total_wins/total_games
+
+
+def getAwayWinRatio():
+    total_wins = 0
+    total_games = 0
+
+    for name in teamNames:
+        total_wins = total_wins + teams_away_prob[name][0]
+        total_games = total_games + teams_away_prob[name][1]
+
+    return total_wins/total_games
+
+
+def getAwayWinRatioTeam(team):
+    if teams_home_prob[team[0]][1] == 0:
+        return 0.5
+
+    return teams_away_prob[team[0]][0]/teams_away_prob[team[0]][1]
+
+
+def getHomeWinRatioTeam(team):
+    if teams_home_prob[team[0]][1] == 0:
+        return 0.5
+
+    return teams_home_prob[team[0]][0] / teams_home_prob[team[0]][1]
+
+
+def printTeamHomeProb():
+    total_wins = 0
+    total_games = 0
+    for name in teamNames:
+        total_wins = total_wins + teams_home_prob[name][0]
+        total_games = total_games + teams_home_prob[name][1]
+        print(name, "\t", teams_home_prob[name])
+
+    print("[Home wins, Home Games] =", "{0:.2f}".format(total_wins/total_games))
+
+
+def printTeamAwayProb():
+    total_wins = 0
+    total_games = 0
+    for name in teamNames:
+        total_wins = total_wins + teams_away_prob[name][0]
+        total_games = total_games + teams_away_prob[name][1]
+        print(name, "\t", teams_away_prob[name])
+
+    print("[Away wins, Away Games] =", "{0:.2f}".format(total_wins / total_games))
+
+
+def win_probability(team1, team2, home_advantage=0):
+    delta_mu = sum(r.mu for r in team1) - (sum(r.mu for r in team2)+home_advantage)
     sum_sigma = sum(r.sigma ** 2 for r in itertools.chain(team1, team2))
     size = len(team1) + len(team2)
     denom = math.sqrt(size * (trueskill.BETA * trueskill.BETA) + sum_sigma)
